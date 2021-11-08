10367f5159c1a7c0ac09adadf1848442acf681fd
SilverNexus <a_daniel.hawkins@yahoo.com>
2021-11-07 20:08:37 -0500

    Add minheap code and the beginnings of a test suite to validate it. Next step is to implement the minheap into monster_compute_path().

---
+++ include/minheap.h
@@ -0,0 +1,39 @@
+#ifndef MINHEAP_H
+#define MINHEAP_H
+
+/*
+ * In order to adequately path for A* search, we will
+ * need a minheap to efficiently handle pathing from the lowest cost
+ * to our destination.
+ *
+ * But, in order to allow for re-use, this is also designed generically.
+ */
+
+/**
+ * 99.9% of the time, you are going to want to interact with the functions provided,
+ * rather the the struct directly.
+ *
+ * If you think you know better than the algorithm, you probably don't. There's a test suite for a reason.
+ */
+typedef struct {
+    // The heap array
+    void **arr;
+    // The in-use length of the min-heap.
+    int len;
+    // The capacity of the min-heap
+    int capacity;
+    // Functon to get the measured quantity from the stored data.
+    // The returned measure is what the min-heap organizes on.
+    int (*get_measure)(void *);
+    // Function pointer to clean up the elements
+    // Can do nothing, which allows for us to store primitive data types,
+    // stack-allocated data, or data that is still in use.
+    void (*element_cleanup)(void *);
+} MinHeap;
+
+MinHeap *minheap_init(int amt, int (*measure_func)(void *), void (*cleanup_func)(void *));
+int minheap_insert(MinHeap *heap, void *ob);
+void *minheap_remove(MinHeap *heap);
+void minheap_free(MinHeap *to_free);
+
+#endif
---
+++ test/unit/common/check_minheap.c
@@ -0,0 +1,125 @@
+/*
+ * This is the unit tests file for common/minheap.c
+ */
+
+#include "minheap.h"
+
+#include <check.h>
+#include <stdlib.h>
+#include <string.h>
+
+static MinHeap *test_heap;
+
+static void setup(void) {
+    cctk_setlog(LOGDIR "/unit/common/minheap.out");
+}
+
+static void teardown(void) {
+    // Clean up the minheap after every test.
+    if (test_heap != NULL)
+        minheap_free(test_heap);
+    test_heap = NULL;
+}
+
+// Define functions that will be needed in order to retrieve values from the heap.
+int int_measure(void *ob) {
+    return *((int *)ob);
+}
+
+// We will also need this in order to qsort the results to ensure it works.
+// This will tell qsort to sort in ascending order.
+int qsort_comp_int(const void *a, const void *b) {
+    return *((int *)a) - *((int *)b);
+}
+
+// Ensure that allocation works.
+START_TEST(test_minheap_allocate) {
+    test_heap = minheap_init(1, int_measure, NULL);
+    fail_unless(test_heap != NULL, "Failed to allocate minheap of size 1.");
+    fail_unless(test_heap->len == 0, "Initialized empty heap has nonzero length.");
+    fail_unless(test_heap->capacity == 1, "Initialized heap has capacity of %d, not 1.", test_heap->capacity);
+}
+END_TEST
+
+START_TEST(test_minheap_overfill) {
+    test_heap = minheap_init(1, int_measure, NULL);
+    int vals[2] = {4, 3};
+    int result;
+    result = minheap_insert(test_heap, &vals[0]);
+    fail_unless(result == 0, "Minheap should have allowed 1 element, but allowed 0.");
+    result = minheap_insert(test_heap, &vals[1]);
+    fail_unless(result == -1, "Minheap of size 1 allowed 2 elements to be added.");
+}
+END_TEST
+
+START_TEST(test_minheap_empty_remove) {
+    test_heap = minheap_init(1, int_measure, NULL);
+    int *result = minheap_remove(test_heap);
+    fail_unless(result == NULL, "Empty heap should not return a non-null value on remove.");
+    int vals[3] = {1, 2, 3};
+    minheap_insert(test_heap, &vals[0]);
+    result = minheap_remove(test_heap);
+    fail_unless(result == &vals[0], "Heap should have returned the only value it was given (%d)", vals[0]);
+    result = minheap_remove(test_heap);
+    fail_unless(result == NULL, "Heap should be empty, yet we returned a value on removal (len=%d)", test_heap->len);
+    minheap_insert(test_heap, &vals[1]);
+    int insresult = minheap_insert(test_heap, &vals[2]);
+    fail_unless(insresult == -1, "Heap should have been full, but insert was allowed (len=%d)", test_heap->len);
+}
+END_TEST
+
+START_TEST(test_minheap_insert_remove) {
+    int vals[30] = {4, 6, 23, 7, 343, 12, 1, 1, 33, 76, 4, 34, 8, 7, 90, 123, 2, 3, 55, 65, 4, 6, 77, 65, 5, 7, 6, 5, 4, 1, 2};
+    int expected[30], actual[30];
+    // Copy the values to the expected array and then qsort them.
+    memcpy(expected, vals, sizeof(int)*30);
+    qsort(expected, 30, sizeof(int), qsort_comp_int);
+
+    // Initialize the minheap.
+    test_heap = minheap_init(30, int_measure, NULL);
+    for (int i = 0; i < 30; ++i) {
+        int result = minheap_insert(test_heap, &vals[i]);
+        fail_unless(result == 0, "Failed to insert to the heap for index %d (%d).", i, vals[i]);
+    }
+    // Now we pull off the heap, which incidentally sorts the values.
+    for (int i = 0; i < 30; ++i) {
+        int *result = minheap_remove(test_heap);
+        fail_unless(result != NULL, "Failed to remove from the heap after %d elements.", i);
+        actual[i] = *result;
+        fail_unless(expected[i] == actual[i], "Heap did not gather data correctly. (%d != %d)", expected[i], actual[i]);
+    }
+}
+END_TEST
+
+static Suite *minheap_suite(void) {
+    Suite *s = suite_create("minheap");
+    TCase *tc_core = tcase_create("Core");
+    tcase_set_timeout(tc_core, 60);
+
+    /*setup and teardown will be called before each test in testcase 'tc_core' */
+    tcase_add_checked_fixture(tc_core, setup, teardown);
+
+    suite_add_tcase(s, tc_core);
+    tcase_add_test(tc_core, test_minheap_allocate);
+    tcase_add_test(tc_core, test_minheap_overfill);
+    tcase_add_test(tc_core, test_minheap_empty_remove);
+    tcase_add_test(tc_core, test_minheap_insert_remove);
+
+    return s;
+}
+
+int main(void) {
+    int nf;
+    Suite *s = minheap_suite();
+    SRunner *sr = srunner_create(s);
+
+    /* to debug, uncomment this line */
+    /*srunner_set_fork_status(sr, CK_NOFORK);*/
+
+    srunner_set_xml(sr, LOGDIR "/unit/common/minheap.xml");
+    srunner_set_log(sr, LOGDIR "/unit/common/minheap.out");
+    srunner_run_all(sr, CK_ENV); /*verbosity from env variable*/
+    nf = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (nf == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
