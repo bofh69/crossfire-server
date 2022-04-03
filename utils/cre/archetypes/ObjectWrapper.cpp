d1c6366bd37d1e9edb8d811af879109ad37e6df4
Nicolas Weeger <ryo_saeba@users.sourceforge.net>
2022-04-03 20:28:45 +0200

    Fix compilation

--- utils/cre/archetypes/ObjectWrapper.cpp
+++ utils/cre/archetypes/ObjectWrapper.cpp
@@ -55,7 +55,7 @@ QString ObjectWrapper::materialName() const
 }
 
 float ObjectWrapper::dps() const {
-    return damage() * abs(speed()) / MOVE_PER_SECOND;
+    return damage() * FABS(speed()) / MOVE_PER_SECOND;
 }
 
 float ObjectWrapper::regen() const {
