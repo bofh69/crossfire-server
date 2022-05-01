ae7a0f37dd5bdb45e8067d48ad6a74134be3df3a
Nicolas Weeger <ryo_saeba@users.sourceforge.net>
2022-05-01 22:07:24 +0200

    Fix documentation

--- include/minheap.h
+++ include/minheap.h
@@ -1,33 +1,31 @@
 #ifndef MINHEAP_H
 #define MINHEAP_H
 
-/*
+/**
  * In order to adequately path for A* search, we will
  * need a minheap to efficiently handle pathing from the lowest cost
  * to our destination.
  *
  * But, in order to allow for re-use, this is also designed generically.
- */
-
-/**
+ *
  * 99.9% of the time, you are going to want to interact with the functions provided,
- * rather the the struct directly.
+ * rather than the struct directly.
  *
  * If you think you know better than the algorithm, you probably don't. There's a test suite for a reason.
  */
 typedef struct {
-    // The heap array
+    /** The heap array. */
     void **arr;
-    // The in-use length of the min-heap.
+    /** The in-use length of the min-heap. */
     int len;
-    // The capacity of the min-heap
+    /** The capacity of the min-heap. */
     int capacity;
-    // Functon to get the measured quantity from the stored data.
-    // The returned measure is what the min-heap organizes on.
+    /** Functon to get the measured quantity from the stored data.
+     * The returned measure is what the min-heap organizes on. */
     int (*get_measure)(const void *);
-    // Function pointer to clean up the elements
-    // Can do nothing, which allows for us to store primitive data types,
-    // stack-allocated data, or data that is still in use.
+    /** Function pointer to clean up the elements.
+     * Can do nothing, which allows for us to store primitive data types,
+     * stack-allocated data, or data that is still in use. */
     void (*element_cleanup)(void *);
 } MinHeap;
 
