0d865bd38fb221c7219d4021d669a2b692b12492
Nicolas Weeger <ryo_saeba@users.sourceforge.net>
2022-05-22 10:23:07 +0200

    Squash unused variable warning

--- test/unit/server/comet_perf.c
+++ test/unit/server/comet_perf.c
@@ -127,6 +127,9 @@ static void check_hp(const char *test, int hp_row[TEST_MAP_SIZE], int hp_diag[TE
 
 #ifdef PRINT_DEBUG_HP
     printf("\nDumping debug hp for test %s\n ", test);
+    (void)hp_row;
+    (void)hp_diag;
+    (void)diff;
 #else
     (void)test;
 #endif
