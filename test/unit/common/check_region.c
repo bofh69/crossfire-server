ce2fd867aefa82d8b69ffce96f1fefd9168ab1bd
Nicolas Weeger <ryo_saeba@users.sourceforge.net>
2022-05-22 13:56:41 +0200

    Add unit tests for region.c

--- test/unit/common/check_region.c
+++ test/unit/common/check_region.c
@@ -1,13 +1,7 @@
 /*
- * static char *rcsid_check_region_c =
- *   "$Id$";
- */
-
-/*
  * CrossFire, A Multiplayer game for X-windows
  *
- * Copyright (C) 2002 Mark Wedel & Crossfire Development Team
- * Copyright (C) 1992 Frank Tore Johansen
+ * Copyright (C) 2022 the Crossfire Development Team
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,17 +26,91 @@
 
 #include <stdlib.h>
 #include <check.h>
+#include <global.h>
+#include <assert.h>
+#include <toolkit_common.h>
 
 void setup(void) {
-    /* put any initialisation steps here, they will be run before each testcase */
+    cctk_setdatadir(SOURCE_ROOT "lib");
+    cctk_setlog(LOGDIR "/unit/common/region.out");
+    init_regions();
 }
 
 void teardown(void) {
     /* put any cleanup steps here, they will be run after each testcase */
 }
 
-START_TEST(test_empty) {
-    /*TESTME test not yet developped*/
+START_TEST(test_get_by_unknown_name) {
+    region *reg = get_region_by_name("undefined?!");
+    fail_unless(reg != NULL, "Must get a region for all names");
+    fail_unless(reg->fallback, "Region must be fallback");
+}
+END_TEST
+
+START_TEST(test_get_by_name) {
+    region *reg = get_region_by_name("scorn");
+    fail_unless(reg != NULL, "Must get a region for scorn");
+    fail_unless(!reg->fallback, "Region must not be fallback");
+}
+END_TEST
+
+START_TEST(test_get_region_from_string_empty) {
+    region *reg = get_region_from_string("");
+    fail_unless(reg != NULL, "Must get a region for empty");
+    fail_unless(!reg->parent, "Region must not have a parent");
+}
+END_TEST
+
+START_TEST(test_get_region_from_string_exact_name) {
+    region *reg = get_region_from_string("darcapcircus");
+    fail_unless(reg != NULL, "Must get a region for darcapcircus");
+    fail_unless(!reg->fallback, "Region must not be fallback");
+}
+END_TEST
+
+START_TEST(test_get_region_from_string_exact_name_wrong_case) {
+    region *reg = get_region_from_string("darcapciRcus");
+    fail_unless(reg != NULL, "Must not get a region for darcapciRcus");
+    fail_unless(!reg->fallback, "Region must not be fallback");
+}
+END_TEST
+
+START_TEST(test_get_region_from_string_partial_name) {
+    region *reg = get_region_from_string("uthv");
+    fail_unless(reg != NULL, "Must get a region for uthv");
+    fail_unless(!reg->fallback, "Region must not be fallback");
+}
+END_TEST
+
+START_TEST(test_get_region_from_string_exact_long_name) {
+    region *reg = get_region_from_string("Team Arena Volcano");
+    fail_unless(reg != NULL, "Must get a region for Team Arena Volcano");
+    fail_unless(!reg->fallback, "Region must not be fallback");
+}
+END_TEST
+
+START_TEST(test_get_region_from_string_partial_long_name) {
+    region *reg = get_region_from_string("lord marksel");
+    fail_unless(reg != NULL, "Must get a region for lord marksel");
+    fail_unless(!reg->fallback, "Region must not be fallback");
+}
+END_TEST
+
+START_TEST(test_check_region_parent) {
+    region *reg = get_region_from_string("scorncounty");
+    fail_unless(reg != NULL, "Must get a region for scorncounty");
+    fail_unless(!reg->fallback, "Region must not be fallback");
+    fail_unless(reg->parent != NULL, "scorncounty must have a parent");
+    fail_unless(strcmp(reg->parent->name, "scorn") == 0, "scorncounty must be child of scorn");
+    fail_unless(reg->parent->parent != NULL, "Scorn must be a child too");
+    fail_unless(strcmp(reg->parent->parent->name, "world") == 0, "scorn must be child of world");
+}
+END_TEST
+
+START_TEST(test_get_region_is_child_of_region) {
+    region *reg1 = get_region_from_string("scorn");
+    region *reg2 = get_region_from_string("scorncounty");
+    fail_unless(region_is_child_of_region(reg2, reg1), "scorncountry should be child of scorn");
 }
 END_TEST
 
@@ -54,7 +122,16 @@ Suite *region_suite(void) {
     tcase_add_checked_fixture(tc_core, setup, teardown);
 
     suite_add_tcase(s, tc_core);
-    tcase_add_test(tc_core, test_empty);
+    tcase_add_test(tc_core, test_get_by_unknown_name);
+    tcase_add_test(tc_core, test_get_by_name);
+    tcase_add_test(tc_core, test_get_region_from_string_empty);
+    tcase_add_test(tc_core, test_get_region_from_string_exact_name);
+    tcase_add_test(tc_core, test_get_region_from_string_exact_name_wrong_case);
+    tcase_add_test(tc_core, test_get_region_from_string_partial_name);
+    tcase_add_test(tc_core, test_get_region_from_string_exact_long_name);
+    tcase_add_test(tc_core, test_get_region_from_string_partial_long_name);
+    tcase_add_test(tc_core, test_check_region_parent);
+    tcase_add_test(tc_core, test_get_region_is_child_of_region);
 
     return s;
 }
