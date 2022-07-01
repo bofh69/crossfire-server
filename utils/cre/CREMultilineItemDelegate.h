8fd4003ed0284831c5ab34bb5c4f95d8f6f1ef2c
Nicolas Weeger <ryo_saeba@users.sourceforge.net>
2022-07-01 18:53:00 +0200

    Add missing 'override'

--- utils/cre/CREMultilineItemDelegate.h
+++ utils/cre/CREMultilineItemDelegate.h
@@ -28,9 +28,9 @@ class CREMultilineItemDelegate : public QStyledItemDelegate
         CREMultilineItemDelegate(QObject* parent, bool asStringList = true, bool trimEmpty = false);
         virtual ~CREMultilineItemDelegate();
 
-        virtual QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const;
-        virtual void setEditorData(QWidget* editor, const QModelIndex& index) const;
-        virtual void setModelData(QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const;
+        virtual QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+        virtual void setEditorData(QWidget* editor, const QModelIndex& index) const override;
+        virtual void setModelData(QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const override;
 
 private:
     bool myAsStringList;
--- utils/cre/CREPrePostConditionDelegate.h
+++ utils/cre/CREPrePostConditionDelegate.h
@@ -56,9 +56,9 @@ public:
     CREPrePostConditionDelegate(QObject* parent, PrePostWidget::Mode mode, const MessageManager* manager);
     virtual ~CREPrePostConditionDelegate();
 
-    virtual QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const;
-    virtual void setEditorData(QWidget* editor, const QModelIndex& index) const;
-    virtual void setModelData(QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const;
+    virtual QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+    virtual void setEditorData(QWidget* editor, const QModelIndex& index) const override;
+    virtual void setModelData(QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const override;
     virtual void updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 
 private:
