ced054464b7a5b8b193c9f2c2834afed7b7e2a1e
Kevin Zheng <kevinz5000@gmail.com>
2023-04-17 16:36:14 -0700

    Fix test_mapsave for unique maps

--- test/performance/test_mapsave.cpp
+++ test/performance/test_mapsave.cpp
@@ -1,3 +1,21 @@
+/**
+ * test_mapsave.cpp -- save stability test
+ *
+ * This file creates a program that loads and saves a map given by the in-game
+ * PATH (e.g. /world/world_105_115). If everything is working, load/save should
+ * not change the file.
+ *
+ * Due to the lack of correct linked list reversing, we load/save twice to get
+ * the same order of objects again.
+ *
+ * For normal (e.g. world) maps, load/save saves to a temporary file, so we can
+ * avoid modifying the original file and diff between the original and saved
+ * temporary file.
+ *
+ * For unique (e.g. apartment) maps, this test SAVES OVER THE ORIGINAL FILE. So
+ * you should make a copy of the original file so you can diff the result.
+ */
+
 #include "global.h"
 
 extern void map_path(const char *map, int flags, char *pathname, size_t bufsize);
@@ -29,7 +47,13 @@ int main(int argc, char *argv[]) {
     char buf[MAX_BUF];
     map_path(m->path, flags, buf, sizeof(buf));
 
-    const char *path_firstpass = strdup(m->tmpname);
+    const char *path_firstpass = NULL;
+    if (m->tmpname != NULL) {
+        // happens if map is unique
+        strdup(m->tmpname);
+    } else {
+        path_firstpass = buf;
+    }
     free_map(m);
     m = mapfile_load_lowlevel(argv[1], path_firstpass, flags);
     if (m == NULL) {
@@ -38,7 +62,9 @@ int main(int argc, char *argv[]) {
     save_map(m, 0);
 
     LOG(llevInfo, "Original:\t%s\n", buf);
-    LOG(llevInfo, "Pass 1:\t%s\n", path_firstpass);
-    LOG(llevInfo, "Pass 2:\t%s\n", m->tmpname);
+    if (!(flags & MAP_PLAYER_UNIQUE)) {
+        LOG(llevInfo, "Pass 1:\t%s\n", path_firstpass);
+        LOG(llevInfo, "Pass 2:\t%s\n", m->tmpname);
+    }
     free_map(m);
 }
