094bb71d6b588cbf0bb9d91a91d3ed90cd16b582
Kevin Zheng <kevinz5000@gmail.com>
2025-06-22 15:11:56 -0700

    Show level for potions

--- common/item.cpp
+++ common/item.cpp
@@ -542,6 +542,7 @@ void query_short_name(const object *op, char *buf, size_t size) {
     case SCROLL:
     case WAND:
     case ROD:
+    case POTION:
         if (QUERY_FLAG(op, FLAG_IDENTIFIED) || QUERY_FLAG(op, FLAG_BEEN_APPLIED)) {
             if (!op->title && (op->inv)) {
                 safe_strcat(buf, " of ", &len, size);
@@ -719,6 +720,7 @@ void query_base_name(const object *op, int plural, char *buf, size_t size) {
     case SCROLL:
     case WAND:
     case ROD:
+    case POTION:
         if (QUERY_FLAG(op, FLAG_IDENTIFIED) || QUERY_FLAG(op, FLAG_BEEN_APPLIED)) {
             if (!op->title && (op->inv)) {
                 safe_strcat(buf, " of ", &len, size);
