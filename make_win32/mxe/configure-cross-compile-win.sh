26f2ffa21cc796fee612e8854b37b9aaca94e4a1
Nicolas Weeger <ryo_saeba@users.sourceforge.net>
2025-07-04 19:21:15 +0200

    Fix MXE cross-build

--- make_win32/mxe/configure-cross-compile-win.sh
+++ make_win32/mxe/configure-cross-compile-win.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-( cd ../.. && sh autogen.sh --host=x86_64-w64-mingw32.static --prefix= "$@")
+( cd ../.. && sh autogen.sh --host=x86_64-w64-mingw32.static --prefix= "$@" --enable-static)
