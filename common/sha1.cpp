db1e744e260f48e5c9752d6e195dec6f9cc49bb0
Sebastian Andersson <sebastian.andersson@lumenradio.com>
2026-05-04 18:53:16 +0200

    Remove confusing copyright header

--- common/sha1.cpp
+++ common/sha1.cpp
@@ -1,15 +1,3 @@
-/*
- * Crossfire -- cooperative multi-player graphical RPG and adventure game
- *
- * Copyright (c) 2026 the Crossfire Development Team
- *
- * Crossfire is free software and comes with ABSOLUTELY NO WARRANTY. You are
- * welcome to redistribute it under certain conditions. For details, please
- * see COPYING and LICENSE.
- *
- * The authors can be reached via e-mail at <crossfire@metalforge.org>.
- */
-
 /**
  * \file
  * SHA-1 hash function implementation (public domain, adapted from Steve
